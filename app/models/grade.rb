ac295d61388ae4dac0a156207ec9e7f8c1f983d1
robertlandreaux <rcl2288@yahoo.com>
2015-06-11 17:29:27 -0400

    ditch n+1 query

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -5,6 +5,10 @@ class Grade < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :user
 
+  def self.for_student(student)
+    where(student: student)
+  end
+
   def self.get_grade(student_id, assignment_id)
     records = Grade.where("student_id"=>student_id,"assignment_id"=>assignment_id).first
   end
